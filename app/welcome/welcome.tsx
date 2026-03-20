f5d25e81dc89748aacc4915574c06341d250939a
Yuta Takahashi <y.highbridge04@gmail.com>
2026-03-21 00:15:41 +0900

    feat: enhance Welcome component layout with additional icons and improved structure

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,12 +1,22 @@
-import { Center, VStack, Stack, Image } from "@chakra-ui/react";
+import { Center, VStack, Stack, Image, HStack, Box } from "@chakra-ui/react";
 
 export function Welcome() {
   return (
     <Center h="100%" w="svw">
       <VStack>
-        <Stack>
+        <Stack direction={{ base: "column", md: "row" }} align="center">
           <Image src="/public/avatar.jpg" alt="avatar" borderRadius="full" />
+          <VStack>
+            <Box>name</Box>
+            <Box>name</Box>
+            <Box>name</Box>
+          </VStack>
         </Stack>
+        <HStack>
+          <Box>icon a</Box>
+          <Box>icon b</Box>
+          <Box>icon c</Box>
+        </HStack>
       </VStack>
     </Center>
   );
