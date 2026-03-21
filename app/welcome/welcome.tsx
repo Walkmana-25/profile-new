7358740b353e84d5297bef41c712495789f0088d
Yuta Takahashi <y.highbridge04@gmail.com>
2026-03-21 12:04:55 +0900

    refactor: improve Welcome component structure and enhance layout with additional text elements

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,15 +1,40 @@
-import { Center, VStack, Stack, Image, HStack, Box } from "@chakra-ui/react";
+import {
+  Center,
+  VStack,
+  Stack,
+  Image,
+  HStack,
+  Box,
+  Text,
+} from "@chakra-ui/react";
 
 export function Welcome() {
   return (
     <Center h="100%" w="svw">
-      <VStack>
-        <Stack direction={{ base: "column", md: "row" }} align="center">
-          <Image src="/public/avatar.jpg" alt="avatar" borderRadius="full" />
+      <VStack gap={50}>
+        <Stack
+          direction={{ base: "column", md: "row" }}
+          align="center"
+          gap={50}
+        >
+          <Image
+            src="/public/avatar.jpg"
+            alt="avatar"
+            borderRadius="full"
+            height={200}
+            width={200}
+          />
           <VStack>
-            <Box>name</Box>
-            <Box>name</Box>
-            <Box>name</Box>
+            <Text textStyle={"5xl"} fontWeight={"bold"}>
+              Yuta Takahashi
+            </Text>
+            <Text textStyle={"2xl"} fontWeight={"medium"}>
+              INIAD -Toyo University-
+            </Text>
+            <Text textStyle={"lg"}>東洋大学 情報連携学部 情報連携学科</Text>
+            <Text textStyle={"lg"}>
+              2025年度 未踏IT人材発掘・育成事業 クリエーター
+            </Text>
           </VStack>
         </Stack>
         <HStack>
